928761a8d58cbfbd84eb1986ab4d073f2d3a9502
Shawn C <scooter4356@gmail.com>
2020-07-11 14:26:06 -0500

    end of 14.3.1-all working

--- CodingEventsDemo/Models/Event.cs
+++ CodingEventsDemo/Models/Event.cs
@@ -9,16 +9,31 @@ namespace CodingEventsDemo.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public int Id {get;}
+        private static int nextId=1;
 
         public Event(string name, string description)
         {
             Name = name;
             Description = description;
+            Id = nextId;
+            nextId++;
         }
 
         public override string ToString()
         {
             return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Event @event &&
+                Id == @event.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
         }
     }
 }
