1219492d753be42e365296bc17c22da19430de49
Shawn C <scooter4356@gmail.com>
2020-07-16 11:25:13 -0500

    end of 15.3-working

--- CodingEventsDemo/ViewModels/AddEventViewModel.cs
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CodingEventsDemo.ViewModels
 {
     public class AddEventViewModel
     {
+        [Required(ErrorMessage ="Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="Name has to be between 3 and 50 chars")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter a desc.")]
+        [StringLength(500, ErrorMessage ="Desc too long")]
         public string Description { get; set; }
+
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
     }
 }
