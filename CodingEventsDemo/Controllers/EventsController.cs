dae58646cab01352c8e900088f220eb788a9909f
Shawn C <scooter4356@gmail.com>
2020-07-22 19:07:49 -0500

    project is hooked to db

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -13,13 +13,22 @@ namespace coding_events_practice.Controllers
 {
     public class EventsController : Controller
     {
+        private EventDbContext context;
+
+        public EventsController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<Event> events = new List<Event>(EventData.GetAll());
+            List<Event> events = context.Events.ToList();
 
             return View(events);
         }
+
+       
 
         public IActionResult Add()
         {
@@ -38,10 +47,11 @@ namespace coding_events_practice.Controllers
                     Name = addEventViewModel.Name,
                     Description = addEventViewModel.Description,
                     ContactEmail = addEventViewModel.ContactEmail,
-                    //Type = addEventViewModel.Type
+                    Type = addEventViewModel.Type
                 };
 
-                EventData.Add(newEvent);
+                context.Events.Add(newEvent);
+                context.SaveChanges();
 
                 return Redirect("/Events");
             }
@@ -51,7 +61,7 @@ namespace coding_events_practice.Controllers
 
         public IActionResult Delete()
         {
-            ViewBag.events = EventData.GetAll();
+            ViewBag.events = context.Events.ToList();
 
             return View();
         }
@@ -61,9 +71,11 @@ namespace coding_events_practice.Controllers
         {
             foreach (int eventId in eventIds)
             {
-                EventData.Remove(eventId);
+                
+                Event theEvent = context.Events.Find(eventId);
+                context.Events.Remove(theEvent);
             }
-
+            context.SaveChanges();
             return Redirect("/Events");
         }
     }
