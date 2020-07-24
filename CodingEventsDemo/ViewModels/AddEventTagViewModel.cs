1318fc2da8916865b6392e2c79b24b0885ae68d8
Shawn C <scooter4356@gmail.com>
2020-07-24 17:34:02 -0500

    working add tags. #ref

---
+++ CodingEventsDemo/ViewModels/AddEventTagViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.ViewModels
+{
+    public class AddEventTagViewModel
+    {
+        [Required(ErrorMessage ="Event is required.")]
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        [Required(ErrorMessage ="Tag is required")]
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+
+        public List<SelectListItem> Tags { get; set; }
+
+
+        public AddEventTagViewModel()
+        {
+
+        }
+
+        public AddEventTagViewModel(Event theEvent, List<Tag> possibleTags)
+        {
+            Tags = new List<SelectListItem>();
+
+            foreach (var tag in possibleTags)
+            {
+                Tags.Add(new SelectListItem
+                {
+                    Value = tag.Id.ToString(),
+                    Text = tag.Name
+                }); 
+            }
+            Event = theEvent;
+        }
+
+    }
+}
